0867afc14eaaf6477b971c8fbf2dc40e6fdb11ac
novadior1k <ricardo.mbfranca@senacsp.edu.br>
2023-04-04 10:56:09 -0300

    ..

--- JavaScripts/toLogin.js
+++
@@ -1,4 +0,0 @@
-// const loginButton = document.querySelector('#loginButton');
-// loginButton.addEventListener('click', () => {
-//   window.location.href = '/login';
-// });
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const ehbs = require('express-handlebars');
 const mysql = require('mysql');
-
+const bcrypt = require('bcryptjs');
 
 // Set the view engine to hbs
 
@@ -25,9 +25,108 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post("/signup", (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirm_password;
+  
+    // Check if name is not empty
+    if (name === "") {
+      return res.status(400).send("Please enter your name.");
+    }
+  
+    // Check if email is valid
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).send("Please enter a valid email address.");
+    }
+  
+    // Check if password is at least 8 characters long
+    if (password.length < 8) {
+      return res.status(400).send("Password must be at least 8 characters long.");
+    }
+  
+    // Check if password and confirm password match
+    if (password !== confirmPassword) {
+      return res.status(400).send("Passwords do not match.");
+    }
+  
+    // If all checks pass, create a new user account
+    
+    // Hash the password
+    const salt = bcrypt.genSaltSync(10);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+    const query = `INSERT INTO users (username, email, password, created_at) VALUES (?, ?, ?, current_timestamp())`;
+    data = [name, email, hashedPassword];
+    connection.query(query, data, (error, results, fields) => {
+    if (error) {
+        if (error.code === "ER_DUP_ENTRY") {
+            // Duplicate email address
+            return res.status(400).send("Email address is already registered.");
+          } else {
+            // Other database error
+            console.error(error);
+            return res.status(500).send("Error registering user.");
+          }
+    } 
+    else {
+      res.render('dashboard');
+    }
+    });
+
+    // render client dashboard
+    
+});
+
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    // Check if email is valid
+    const emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).send("Please enter a valid email address");
+    }
+  
+    // Find the user with the given email
+    const query = 'SELECT * FROM users WHERE email = ?';
+    connection.query(query, email, (error, results, fields) => {
+      if (error) {
+        console.error(error);
+        res.status(500).send('Error logging in');
+      } else {
+        if (results.length === 0) {
+          // User not found
+          res.status(400).send('User not found');
+        } else {
+          // Compare the password with the hashed password in the database
+          const hashedPassword = results[0].password;
+          const isPasswordValid = bcrypt.compareSync(password, hashedPassword);
+          if (!isPasswordValid) {
+            // Incorrect password
+            res.status(400).send('Incorrect password');
+          } else {
+            // Login successful
+            res.render('dashboard')
+          }
+        }
+      }
+    });
+  });
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+});
+  
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -44,7 +143,9 @@ connection.connect((err) => {
     else {
         console.log('Connected to database');
     }
-    app.listen(port , () => {
-        console.log(`Server is running on port ${port}`);
-    }); 
 });
+
+// server
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+}); 
